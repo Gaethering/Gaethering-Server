3a6bf57e86366703c0fdfdcf50c2bba49cebe7ee
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-05 11:17:42 +0900

    Initial Commit

---
+++ src/main/java/com/gaethering/gaetheringserver/GaetheringServerApplication.java
@@ -0,0 +1,13 @@
+package com.gaethering.gaetheringserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaetheringServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GaetheringServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaethering/gaetheringserver/GaetheringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaethering.gaetheringserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaetheringServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
