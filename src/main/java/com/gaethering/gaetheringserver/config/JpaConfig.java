6497d52a7f315c7586450e257183aa8878c575e9
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-05 12:02:17 +0900

    feat: Jpa, Transaction config 추가

---
+++ src/main/java/com/gaethering/gaetheringserver/config/JpaConfig.java
@@ -0,0 +1,11 @@
+package com.gaethering.gaetheringserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
