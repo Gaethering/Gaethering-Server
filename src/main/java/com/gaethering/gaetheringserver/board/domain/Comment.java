85161841a0879a1e01ce84637cc79f6eec3e89ab
EunjunOh123 <grace99985@gmail.com>
2022-12-05 21:10:56 +0900

    feat: 댓글 엔티티 생성

---
+++ src/main/java/com/gaethering/gaetheringserver/board/domain/Comment.java
@@ -0,0 +1,39 @@
+package com.gaethering.gaetheringserver.board.domain;
+
+import com.gaethering.gaetheringserver.core.domain.BaseTimeEntity;
+import com.gaethering.gaetheringserver.member.domain.Member;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", updatable = false)
+    private Post post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member member;
+
+    public void setPost (Post post) {
+        this.post = post;
+    }
+
+    public void setMember (Member member) {
+        this.member = member;
+    }
+}
