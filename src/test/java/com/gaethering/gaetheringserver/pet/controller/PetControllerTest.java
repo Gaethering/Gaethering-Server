84753389a0dadc890e6851e53e90cde6a91d2cdf
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-07 01:32:19 +0900

    test(MemberControllerTest): 대표 반려동물 설정 api 테스트 코드 작성

--- src/test/java/com/gaethering/gaetheringserver/pet/controller/PetControllerTest.java
+++ src/test/java/com/gaethering/gaetheringserver/pet/controller/PetControllerTest.java
@@ -1,13 +1,12 @@
-package com.gaethering.gaetheringserver.member.controller;
+package com.gaethering.gaetheringserver.pet.controller;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.gaethering.gaetheringserver.member.service.MemberService;
+import com.gaethering.gaetheringserver.pet.service.PetService;
 import java.security.Principal;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -18,33 +17,29 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(MemberController.class)
-class MemberControllerTest {
+@WebMvcTest(PetController.class)
+class PetControllerTest {
 
     @MockBean
-    private MemberService memberService;
+    private PetService petService;
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     @WithMockUser
-    public void modifyMemberNickname() throws Exception {
+    public void setRepresentativePet() throws Exception {
         //given
         String email = "test@test.com";
-        String nickname = "modifiedNickname";
         Principal principal = Mockito.mock(Principal.class);
         given(principal.getName()).willReturn(email);
 
         //when
         //then
-        mockMvc.perform(patch("/api/mypage/nickname")
+        mockMvc.perform(patch("/api/mypage/pets/1/representative")
                 .contentType(MediaType.APPLICATION_JSON)
-                .with(csrf())
-                .content(nickname))
+                .with(csrf()))
             .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.nickname").value(nickname));
+            .andExpect(status().isOk());
     }
-
 }
