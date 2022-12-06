1e37166bbc80e1e2faf6476fdea15bcdd5aa85b4
EunjunOh123 <grace99985@gmail.com>
2022-12-06 11:21:58 +0900

    feat: black list 구현 위한 redis util 메서드 추가

--- src/main/java/com/gaethering/gaetheringserver/util/RedisUtil.java
+++ src/main/java/com/gaethering/gaetheringserver/util/RedisUtil.java
@@ -1,8 +1,12 @@
 package com.gaethering.gaetheringserver.util;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class RedisUtil {
 
     private final StringRedisTemplate redisTemplate;
+    private final RedisTemplate<String, Object> redisBlackListTemplate;
 
     public String getData(String key) {
         return redisTemplate.opsForValue().get(key);
@@ -22,5 +27,12 @@ public class RedisUtil {
     public void deleteData(String key) {
         redisTemplate.delete(key);
     }
+    public void setBlackList(String key, Object data, Long milliSeconds) {
+        redisBlackListTemplate.setValueSerializer(new Jackson2JsonRedisSerializer(data.getClass()));
+        redisBlackListTemplate.opsForValue().set(key, data, milliSeconds, TimeUnit.MILLISECONDS);
+    }
 
+    public boolean hasKeyBlackList(String key) {
+        return redisBlackListTemplate.hasKey(key);
+    }
 }
