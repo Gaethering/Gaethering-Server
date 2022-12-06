8999708bd4920c51e7db4f924b621302baba6101
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-07 00:07:41 +0900

    feat(MemberController): 회원 닉네임 수정 api 구현

--- src/main/java/com/gaethering/gaetheringserver/member/controller/MemberController.java
+++ src/main/java/com/gaethering/gaetheringserver/member/controller/MemberController.java
@@ -3,25 +3,28 @@ package com.gaethering.gaetheringserver.member.controller;
 import com.gaethering.gaetheringserver.member.dto.ConfirmEmailRequest;
 import com.gaethering.gaetheringserver.member.dto.ConfirmEmailResponse;
 import com.gaethering.gaetheringserver.member.dto.EmailAuthRequest;
+import com.gaethering.gaetheringserver.member.dto.ModifyMemberNicknameResponse;
 import com.gaethering.gaetheringserver.member.dto.SignUpRequest;
 import com.gaethering.gaetheringserver.member.service.MemberService;
+import java.security.Principal;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("${api-prefix}/members")
+@RequestMapping("${api-prefix}")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/sign-up")
+    @PostMapping("/members/sign-up")
     public ResponseEntity<Void> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
 
         memberService.signUp(signUpRequest);
@@ -29,14 +32,14 @@ public class MemberController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @PostMapping("/email-auth")
+    @PostMapping("/members/email-auth")
     public ResponseEntity<Void> sendEmailAuthCode(@RequestBody EmailAuthRequest emailAuthRequest) {
         memberService.sendEmailAuthCode(emailAuthRequest.getEmail());
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PostMapping("/email-confirm")
+    @PostMapping("/members/email-confirm")
     public ResponseEntity<ConfirmEmailResponse> confirmEmailAuthCode(
         @RequestBody ConfirmEmailRequest confirmEmailRequest
     ) {
@@ -45,5 +48,13 @@ public class MemberController {
         return ResponseEntity.ok(ConfirmEmailResponse.builder()
             .emailAuth(true)
             .build());
+    }
+
+    @PatchMapping("/mypage/nickname")
+    public ResponseEntity<ModifyMemberNicknameResponse> modifyMemberNickname(
+        @RequestBody String nickname, Principal principal) {
+        String email = principal.getName();
+        memberService.modifyNickname(email, nickname);
+        return ResponseEntity.ok(new ModifyMemberNicknameResponse(nickname));
     }
 }
