db069d18b35e398ba9035029bcae846b02ae3194
EunjunOh123 <grace99985@gmail.com>
2022-12-06 14:27:34 +0900

    feat: 로그인 요청 및 응답 dto 작성

---
+++ src/main/java/com/gaethering/gaetheringserver/member/dto/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.gaethering.gaetheringserver.member.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class LoginRequest {
+
+    private String email;
+
+    private String password;
+}
