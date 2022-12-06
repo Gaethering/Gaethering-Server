8e55ebd6958e75c2c116b76dbcc34424b772a8b3
EunjunOh123 <grace99985@gmail.com>
2022-12-06 15:12:36 +0900

    feat: Jwt가 유효한 토큰인지 인증하기 위한 Filter 클래스 생성

---
+++ src/main/java/com/gaethering/gaetheringserver/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,41 @@
+package com.gaethering.gaetheringserver.filter;
+
+import com.gaethering.gaetheringserver.util.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        if (!request.getRequestURI().contains("/login")
+                && !request.getRequestURI().contains("/exception")) {
+
+            String token = jwtProvider.resolveToken(request);
+
+            if (token != null && jwtProvider.validateToken(token)) {
+
+                Authentication authentication = jwtProvider.getAuthentication(token);
+
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
