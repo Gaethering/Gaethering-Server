173a8da8e97924c0dbb4ce84653faffa6fa7ea95
EunjunOh123 <grace99985@gmail.com>
2022-12-08 00:22:18 +0900

    fix: 어노테이션 수정

--- src/main/java/com/gaethering/gaetheringserver/member/dto/LoginResponse.java
+++ src/main/java/com/gaethering/gaetheringserver/member/dto/LoginResponse.java
@@ -2,8 +2,10 @@ package com.gaethering.gaetheringserver.member.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginResponse {
 
