89b7ca26de87359c1617eb91e0c5576d4c02466e
naekang <rlawlsgh6306@gmail.com>
2022-12-07 11:26:11 +0900

    refactor: Custom annotation import static화

--- src/main/java/com/gaethering/gaetheringserver/core/validator/LocalDateValid.java
+++ src/main/java/com/gaethering/gaetheringserver/core/validator/LocalDateValid.java
@@ -1,17 +1,19 @@
 package com.gaethering.gaetheringserver.core.validator;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
 @Documented
 @Constraint(validatedBy = LocalDateValidator.class)
-@Target({ElementType.METHOD, ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME)
+@Target({METHOD, FIELD})
+@Retention(RUNTIME)
 public @interface LocalDateValid {
 
     String message() default "8자리의 yyyy-MM-dd 형식이어야 합니다.";
