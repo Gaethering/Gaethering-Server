446b737fbc8c0b4dd23e4d8d5c985379ac83abce
naekang <rlawlsgh6306@gmail.com>
2022-12-07 11:22:27 +0900

    feat: 커스텀 에러코드 추가

--- src/main/java/com/gaethering/gaetheringserver/member/exception/errorcode/MemberErrorCode.java
+++ src/main/java/com/gaethering/gaetheringserver/member/exception/errorcode/MemberErrorCode.java
@@ -10,7 +10,8 @@ public enum MemberErrorCode {
     INVALID_EMAIL_AUTH_CODE("E901", "이메일 인증 코드가 유효하지 않습니다."),
     DUPLICATED_EMAIL("E902", "중복된 이메일입니다."),
     FAILED_SEND_EMAIL("E903", "이메일 전송에 실패하였습니다."),
-    NOT_MATCH_PASSWORD("E904", "비밀번호가 서로 일치하지 않습니다.");
+    NOT_MATCH_PASSWORD("E904", "비밀번호가 서로 일치하지 않습니다."),
+    INVALID_ARGUMENT("E905", "유효하지 않은 요청 형식입니다.");
 
     private final String code;
     private final String message;
