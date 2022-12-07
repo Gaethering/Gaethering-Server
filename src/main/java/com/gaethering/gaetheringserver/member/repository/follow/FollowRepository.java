240b9dcdcf92b1b0e23fb2b058fa1ad8555f4a0d
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-08 03:14:09 +0900

    feat(FollowRepository): Followee로 follow 찾기 구현

--- src/main/java/com/gaethering/gaetheringserver/member/repository/follow/FollowRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/member/repository/follow/FollowRepository.java
@@ -2,6 +2,7 @@ package com.gaethering.gaetheringserver.member.repository.follow;
 
 import com.gaethering.gaetheringserver.member.domain.Follow;
 import com.gaethering.gaetheringserver.member.domain.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
@@ -9,4 +10,6 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     Long countByFollowee(Member member);
 
     Long countByFollower(Member member);
+
+    List<Follow> findByFollowee(Member member);
 }
