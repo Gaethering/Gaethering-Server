2f0c7f8dccacb00a6e91ca9e336cb4c7be9da30b
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-07 09:57:23 +0900

    test(MemberControllerTest): 회원(타인) 프로필 조회 api 테스트 코드 작성

--- src/test/java/com/gaethering/gaetheringserver/member/controller/MemberControllerTest.java
+++ src/test/java/com/gaethering/gaetheringserver/member/controller/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.gaethering.gaetheringserver.member.controller;
 
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -8,6 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.gaethering.gaetheringserver.member.dto.OtherProfileResponse;
 import com.gaethering.gaetheringserver.member.dto.OwnProfileResponse;
 import com.gaethering.gaetheringserver.member.dto.ProfilePetResponse;
 import com.gaethering.gaetheringserver.member.service.MemberProfileService;
@@ -41,14 +43,13 @@ class MemberControllerTest {
     @WithMockUser
     public void getOwnProfile() throws Exception {
         //given
-        String email = "test@test.com";
         ProfilePetResponse petResponse = ProfilePetResponse.builder()
             .id(1L)
             .name("pet")
             .isRepresentative(true)
             .build();
         OwnProfileResponse ownProfileResponse = OwnProfileResponse.builder()
-            .email(email)
+            .email("test@test.com")
             .nickname("nickname")
             .phoneNumber("010-0000-0000")
             .gender(Gender.MALE)
@@ -59,7 +60,7 @@ class MemberControllerTest {
             .pets(List.of(petResponse))
             .build();
         Principal principal = Mockito.mock(Principal.class);
-        given(principal.getName()).willReturn(email);
+        given(principal.getName()).willReturn(ownProfileResponse.getEmail());
         given(memberProfileService.getOwnProfile(anyString()))
             .willReturn(ownProfileResponse);
 
@@ -82,6 +83,52 @@ class MemberControllerTest {
                 String.valueOf(ownProfileResponse.getFollowingCount())))
             .andExpect(
                 jsonPath("$.petCount").value(String.valueOf(ownProfileResponse.getPetCount())))
+            .andExpect(jsonPath("$.pets[0].id").value(String.valueOf(petResponse.getId())))
+            .andExpect(jsonPath("$.pets[0].name").value(petResponse.getName()))
+            .andExpect(jsonPath("$.pets[0].representative").value(
+                String.valueOf(petResponse.isRepresentative())));
+    }
+
+    @Test
+    @WithMockUser
+    public void getOtherProfile() throws Exception {
+        //given
+        ProfilePetResponse petResponse = ProfilePetResponse.builder()
+            .id(1L)
+            .name("pet")
+            .isRepresentative(true)
+            .build();
+        OtherProfileResponse otherProfile = OtherProfileResponse.builder()
+            .email("test@test.com")
+            .nickname("nickname")
+            .gender(Gender.MALE)
+            .mannerDegree(36.5f)
+            .followerCount(10L)
+            .followingCount(10L)
+            .petCount(5)
+            .pets(List.of(petResponse))
+            .build();
+        given(memberProfileService.getOtherProfile(anyLong()))
+            .willReturn(otherProfile);
+
+        //when
+        //then
+        mockMvc.perform(get("/api/members/1/profile")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf()))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.email").value(otherProfile.getEmail()))
+            .andExpect(jsonPath("$.nickname").value(otherProfile.getNickname()))
+            .andExpect(jsonPath("$.gender").value(otherProfile.getGender().toString()))
+            .andExpect(jsonPath("$.mannerDegree").value(
+                String.valueOf(otherProfile.getMannerDegree())))
+            .andExpect(jsonPath("$.followerCount").value(
+                String.valueOf(otherProfile.getFollowerCount())))
+            .andExpect(jsonPath("$.followerCount").value(
+                String.valueOf(otherProfile.getFollowingCount())))
+            .andExpect(
+                jsonPath("$.petCount").value(String.valueOf(otherProfile.getPetCount())))
             .andExpect(jsonPath("$.pets[0].id").value(String.valueOf(petResponse.getId())))
             .andExpect(jsonPath("$.pets[0].name").value(petResponse.getName()))
             .andExpect(jsonPath("$.pets[0].representative").value(
