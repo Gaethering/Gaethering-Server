b951f658d87ce2ca155ba3e6a21ab9e5d1297ebd
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-08 03:16:33 +0900

    feat(FollowService, FollowServiceImpl): 팔로워 찾기 구현

--- src/main/java/com/gaethering/gaetheringserver/member/service/FollowService.java
+++ src/main/java/com/gaethering/gaetheringserver/member/service/FollowService.java
@@ -1,6 +1,11 @@
 package com.gaethering.gaetheringserver.member.service;
 
+import com.gaethering.gaetheringserver.member.dto.FollowResponse;
+import java.util.List;
+
 public interface FollowService {
 
     boolean createFollow(String followerEmail, Long followeeId);
+
+    List<FollowResponse> getFollower(Long memberId);
 }
--- src/main/java/com/gaethering/gaetheringserver/member/service/FollowServiceImpl.java
+++ src/main/java/com/gaethering/gaetheringserver/member/service/FollowServiceImpl.java
@@ -2,9 +2,12 @@ package com.gaethering.gaetheringserver.member.service;
 
 import com.gaethering.gaetheringserver.member.domain.Follow;
 import com.gaethering.gaetheringserver.member.domain.Member;
+import com.gaethering.gaetheringserver.member.dto.FollowResponse;
 import com.gaethering.gaetheringserver.member.exception.MemberNotFoundException;
 import com.gaethering.gaetheringserver.member.repository.follow.FollowRepository;
 import com.gaethering.gaetheringserver.member.repository.member.MemberRepository;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,5 +29,14 @@ public class FollowServiceImpl implements FollowService {
             .orElseThrow(MemberNotFoundException::new);
         followRepository.save(Follow.builder().follower(follower).followee(followee).build());
         return true;
+    }
+
+    @Override
+    public List<FollowResponse> getFollower(Long memberId) {
+        Member member = memberRepository.findById(memberId)
+            .orElseThrow(MemberNotFoundException::new);
+        List<Follow> follows = followRepository.findByFollowee(member);
+        return follows.stream()
+            .map(follow -> FollowResponse.of(follow.getFollower())).collect(Collectors.toList());
     }
 }
