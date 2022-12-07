605bc955bd8a454cfb51e082b014c5c81acd3dbd
EunjunOh123 <grace99985@gmail.com>
2022-12-08 00:12:02 +0900

    fix: 어노테이션 수정

--- src/main/java/com/gaethering/gaetheringserver/member/dto/LoginRequest.java
+++ src/main/java/com/gaethering/gaetheringserver/member/dto/LoginRequest.java
@@ -1,12 +1,9 @@
 package com.gaethering.gaetheringserver.member.dto;
 
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class LoginRequest {
 
