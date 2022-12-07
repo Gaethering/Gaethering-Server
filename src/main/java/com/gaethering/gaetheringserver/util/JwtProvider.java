26c59f8b5b178657c6043467e25df2ce32d8deb8
EunjunOh123 <grace99985@gmail.com>
2022-12-07 22:42:06 +0900

    feat: access/refresh token 생성 메서드 구현

--- src/main/java/com/gaethering/gaetheringserver/util/JwtProvider.java
+++ src/main/java/com/gaethering/gaetheringserver/util/JwtProvider.java
@@ -1,5 +1,8 @@
 package com.gaethering.gaetheringserver.util;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.Base64;
+import java.util.Date;
 
 @Slf4j
 @Component
@@ -30,6 +34,26 @@ public class JwtProvider {
     @PostConstruct
     protected void init() {
         key = Base64.getEncoder().encodeToString(key.getBytes()); // Base64 인코딩
+    }
+
+    public String createAccessToken(String email, Long tokenValid) {
+        Claims claims = Jwts.claims().setSubject(email); // JWT payload 에 저장되는 정보단위
+        Date date = new Date();
+        return Jwts.builder()
+                .setClaims(claims)  // 정보 저장
+                .setIssuedAt(date) // 토큰 발행 시간 정보
+                .setExpiration(new Date(date.getTime() + tokenValid)) // set Expire Time
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
+    }
+
+    public String createRefreshToken(Long tokenValid) {
+        Date date = new Date();
+        return Jwts.builder()
+                .setIssuedAt(date) // 토큰 발행 시간 정보
+                .setExpiration(new Date(date.getTime() + tokenValid)) // set Expire Time
+                .signWith(SignatureAlgorithm.HS256, key)
+                .compact();
     }
 
 }
