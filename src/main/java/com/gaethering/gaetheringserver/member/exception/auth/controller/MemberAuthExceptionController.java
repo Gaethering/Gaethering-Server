f6618f725796d9df0f003e6404414357fda3642a
EunjunOh123 <grace99985@gmail.com>
2022-12-07 23:06:59 +0900

    feat: 인증 및 인가 실패 시 리다이렉트한 곳에서 예외 던지기

---
+++ src/main/java/com/gaethering/gaetheringserver/member/exception/auth/controller/MemberAuthExceptionController.java
@@ -0,0 +1,23 @@
+package com.gaethering.gaetheringserver.member.exception.auth.controller;
+
+import com.gaethering.gaetheringserver.member.exception.auth.AuthenticationFailException;
+import com.gaethering.gaetheringserver.member.exception.auth.AuthorizationFailException;
+import com.gaethering.gaetheringserver.member.exception.errorcode.MemberErrorCode;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exception")
+public class MemberAuthExceptionController {
+
+    @GetMapping("/accessDenied")
+    public String accessDenied () {
+        throw new AuthorizationFailException(MemberErrorCode.FAIL_TO_AUTHORIZATION);
+    }
+
+    @GetMapping("/authenticationFailed")
+    public String authenticationFailed () {
+        throw new AuthenticationFailException(MemberErrorCode.FAIL_TO_AUTHENTICATION);
+    }
+}
