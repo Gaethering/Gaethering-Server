66680620ca3ea2c79a18ed9ac3eb55fc981be93d
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-09 00:37:16 +0900

    feat(GlobalExceptionHandler): GlobalExceptionHandler의 코드 중복을 줄이기 위해 BusinessException에 대한 처리로 수정

--- src/main/java/com/gaethering/gaetheringserver/core/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gaethering/gaetheringserver/core/exception/GlobalExceptionHandler.java
@@ -1,9 +1,7 @@
 package com.gaethering.gaetheringserver.core.exception;
 
-import com.gaethering.gaetheringserver.member.exception.MemberException;
 import com.gaethering.gaetheringserver.member.exception.auth.MemberAuthException;
 import com.gaethering.gaetheringserver.member.exception.errorcode.MemberErrorCode;
-import com.gaethering.gaetheringserver.pet.exception.PetException;
 import java.util.Objects;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,60 +15,40 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MemberException.class)
-    public ResponseEntity<ErrorResponse> handleMemberException(MemberException e) {
-
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
         ErrorResponse response = ErrorResponse.builder()
             .code(e.getErrorCode().getCode())
             .message(e.getMessage())
             .build();
-
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MailSendException.class)
     public ResponseEntity<ErrorResponse> handleMailSendException(MailSendException e) {
-
         ErrorResponse response = ErrorResponse.builder()
             .code(MemberErrorCode.FAILED_SEND_EMAIL.getCode())
             .message(MemberErrorCode.FAILED_SEND_EMAIL.getMessage())
             .build();
-
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(PetException.class)
-    public ResponseEntity<ErrorResponse> handlePetException(PetException e) {
-
-        ErrorResponse response = ErrorResponse.builder()
-            .code(e.getErrorCode().getCode())
-            .message(e.getMessage())
-            .build();
-
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
-        MethodArgumentNotValidException e
-    ) {
-
+        MethodArgumentNotValidException e) {
         ErrorResponse response = ErrorResponse.builder()
             .code(MemberErrorCode.INVALID_ARGUMENT.getCode())
             .message(Objects.requireNonNull(e.getFieldError()).getDefaultMessage())
             .build();
-
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MemberAuthException.class)
     public ResponseEntity<ErrorResponse> handleMemberAuthException(MemberAuthException e) {
-
         ErrorResponse response = ErrorResponse.builder()
-                .code(e.getErrorCode().getCode())
-                .message(e.getMessage())
-                .build();
-
+            .code(e.getErrorCode().getCode())
+            .message(e.getMessage())
+            .build();
         return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 
