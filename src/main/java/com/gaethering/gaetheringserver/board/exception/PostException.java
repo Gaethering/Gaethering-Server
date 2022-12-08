4c2162be01a2b4951b4bce90327a906520ea1125
EunjunOh123 <grace99985@gmail.com>
2022-12-09 00:17:20 +0900

    feat: 게시판 관련 부모 예외 클래스 생성

---
+++ src/main/java/com/gaethering/gaetheringserver/board/exception/PostException.java
@@ -0,0 +1,15 @@
+package com.gaethering.gaetheringserver.board.exception;
+
+import com.gaethering.gaetheringserver.board.exception.errorCode.PostErrorCode;
+import lombok.Getter;
+
+@Getter
+public class PostException extends RuntimeException{
+
+    private final PostErrorCode postErrorCode;
+
+    protected PostException (PostErrorCode postErrorCode) {
+        super(postErrorCode.getMessage());
+        this.postErrorCode = postErrorCode;
+    }
+}
