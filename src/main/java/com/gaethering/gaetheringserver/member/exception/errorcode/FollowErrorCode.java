accfd572cececbc733d32bbd8bd06ee694ea7db2
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-09 00:32:45 +0900

    feat(FollowErrorCode): 팔로우 에러 코드를 ErrorCode 인터페이스를 구현하도록 설정

--- src/main/java/com/gaethering/gaetheringserver/member/exception/errorcode/FollowErrorCode.java
+++ src/main/java/com/gaethering/gaetheringserver/member/exception/errorcode/FollowErrorCode.java
@@ -1,11 +1,12 @@
 package com.gaethering.gaetheringserver.member.exception.errorcode;
 
+import com.gaethering.gaetheringserver.core.exception.ErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public enum FollowErrorCode {
+public enum FollowErrorCode implements ErrorCode {
     FOLLOW_NOT_FOUND("E002", "해당 하는 팔로우 관계가 존재하지 않습니다.");
 
     private final String code;
