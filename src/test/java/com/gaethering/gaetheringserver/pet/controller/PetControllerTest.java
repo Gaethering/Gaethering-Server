7da05c66913128b2ced07e800908a16c0a8e0c05
bokyung <qhrud7915@naver.com>
2022-12-08 22:58:13 +0900

    feat: 반려동물 프로필 수정, 삭제 Controller 테스트코드 작성

--- src/test/java/com/gaethering/gaetheringserver/pet/controller/PetControllerTest.java
+++ src/test/java/com/gaethering/gaetheringserver/pet/controller/PetControllerTest.java
@@ -1,8 +1,10 @@
 package com.gaethering.gaetheringserver.pet.controller;
 
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -13,6 +15,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gaethering.gaetheringserver.member.type.Gender;
 import com.gaethering.gaetheringserver.pet.dto.PetImageUpdateResponse;
 import com.gaethering.gaetheringserver.pet.dto.PetProfileResponse;
+import com.gaethering.gaetheringserver.pet.dto.PetProfileUpdateRequest;
 import com.gaethering.gaetheringserver.pet.service.PetService;
 import java.security.Principal;
 import java.time.LocalDate;
@@ -114,5 +117,59 @@ class PetControllerTest {
             .andExpect(jsonPath("$.description").value(petProfile.getDescription()))
             .andExpect(jsonPath("$.imageUrl").value(petProfile.getImageUrl()))
             .andDo(print()).andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    public void updatePetProfile() throws Exception {
+        //given
+        PetProfileUpdateRequest request = PetProfileUpdateRequest.builder()
+            .weight(3.5f)
+            .isNeutered(true)
+            .description("귀여워요")
+            .build();
+        PetProfileResponse response = PetProfileResponse.builder()
+            .name("해")
+            .birth(LocalDate.parse("2021-12-01"))
+            .gender(Gender.valueOf("FEMALE"))
+            .breed("말티즈")
+            .weight(3.5f)
+            .isNeutered(true)
+            .description("귀여워요")
+            .imageUrl("https://test")
+            .build();
+        given(petService.updatePetProfile(1L, request.getWeight(), request.isNeutered(), request.getDescription()))
+            .willReturn(response);
+
+        String requestString = objectMapper.writeValueAsString(request);
+
+        //when
+        //then
+        mockMvc.perform(patch("/api/mypage/pets/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf())
+                .content(requestString))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    public void deletePetProfile() throws Exception {
+        //given
+        String email = "test@test.com";
+        Principal principal = Mockito.mock(Principal.class);
+        given(principal.getName()).willReturn(email);
+
+        given(petService.deletePetProfile(anyString(), anyLong()))
+            .willReturn(true);
+
+        //when
+        //then
+        mockMvc.perform(delete("/api/mypage/pets/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .with(csrf()))
+            .andDo(print())
+            .andExpect(status().isOk());
     }
 }
