bb4bf8c442f2019b2187e6a2ad1dc6128caa045a
bokyung <qhrud7915@naver.com>
2022-12-08 22:53:06 +0900

    refactor: PetProfileUpdateRequest validation 수정

--- src/main/java/com/gaethering/gaetheringserver/pet/dto/PetProfileUpdateRequest.java
+++ src/main/java/com/gaethering/gaetheringserver/pet/dto/PetProfileUpdateRequest.java
@@ -1,19 +1,22 @@
 package com.gaethering.gaetheringserver.pet.dto;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class PetProfileUpdateRequest {
 
-	@NotBlank(message = "반려동물 몸무게는 필수 입력 사항입니다.")
+	@NotNull(message = "반려동물 몸무게는 필수 입력 사항입니다.")
 	private float weight;
 
-	@NotBlank(message = "중성화 여부는 필수 입력 사항입니다.")
+	@NotNull(message = "중성화 여부는 필수 입력 사항입니다.")
 	private boolean isNeutered;
 
 	@NotBlank(message = "반려동물 상세설명은 필수 입력 사항입니다.")
