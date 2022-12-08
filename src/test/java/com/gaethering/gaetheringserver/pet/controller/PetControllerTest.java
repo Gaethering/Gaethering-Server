0b26f3ab7e0f03d0a6b31d749551280ddc307796
bokyung <qhrud7915@naver.com>
2022-12-08 22:56:36 +0900

    feat: 반려동물 이미지 수정 Controller 테스트코드 작성

--- src/test/java/com/gaethering/gaetheringserver/pet/controller/PetControllerTest.java
+++ src/test/java/com/gaethering/gaetheringserver/pet/controller/PetControllerTest.java
@@ -1,22 +1,36 @@
 package com.gaethering.gaetheringserver.pet.controller;
 
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gaethering.gaetheringserver.member.type.Gender;
+import com.gaethering.gaetheringserver.pet.dto.PetImageUpdateResponse;
+import com.gaethering.gaetheringserver.pet.dto.PetProfileResponse;
+import com.gaethering.gaetheringserver.pet.dto.PetProfileUpdateRequest;
 import com.gaethering.gaetheringserver.pet.service.PetService;
 import java.security.Principal;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(PetController.class)
 @ActiveProfiles("test")
@@ -27,6 +41,9 @@ class PetControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     @WithMockUser
@@ -41,6 +58,40 @@ class PetControllerTest {
         mockMvc.perform(patch("/api/mypage/pets/1/representative")
                 .contentType(MediaType.APPLICATION_JSON)
                 .with(csrf()))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser
+    void updatePetImage() throws Exception {
+        // given
+        String filename = "test.png";
+        String contentType = "image/png";
+
+        MockMultipartFile image = new MockMultipartFile("image", filename, contentType,
+            "test".getBytes());
+        PetImageUpdateResponse response = PetImageUpdateResponse.builder()
+            .imageUrl("https://test").build();
+
+        given(petService.updatePetImage(1L, image))
+            .willReturn(PetImageUpdateResponse.builder().imageUrl(response.getImageUrl()).build());
+
+        // when
+        // then
+        MockMultipartHttpServletRequestBuilder builder =
+            MockMvcRequestBuilders.multipart("/api/mypage/pets/1/image");
+
+        builder.with(request -> {
+            request.setMethod("PATCH");
+            return request;
+        });
+
+        mockMvc.perform(
+                builder
+                    .file(image)
+                    .with(csrf())
+            ).andExpect(jsonPath("$.imageUrl").value(response.getImageUrl()))
             .andDo(print())
             .andExpect(status().isOk());
     }
