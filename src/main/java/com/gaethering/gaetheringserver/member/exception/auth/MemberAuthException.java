2aa967974d06b9412801cb88cf33e60c2e840d28
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-09 00:36:05 +0900

    feat(MemberAuthException): MemberAuthException이 BusinessException을 상속 받도록 설정

--- src/main/java/com/gaethering/gaetheringserver/member/exception/auth/MemberAuthException.java
+++ src/main/java/com/gaethering/gaetheringserver/member/exception/auth/MemberAuthException.java
@@ -1,14 +1,15 @@
 package com.gaethering.gaetheringserver.member.exception.auth;
 
+import com.gaethering.gaetheringserver.core.exception.BusinessException;
 import com.gaethering.gaetheringserver.member.exception.errorcode.MemberErrorCode;
 import lombok.Getter;
 
 @Getter
-public class MemberAuthException extends RuntimeException {
+public class MemberAuthException extends BusinessException {
     private final MemberErrorCode errorCode;
 
     public MemberAuthException(MemberErrorCode memberErrorCode) {
-        super(memberErrorCode.getMessage());
+        super(memberErrorCode);
         this.errorCode = memberErrorCode;
     }
 }
