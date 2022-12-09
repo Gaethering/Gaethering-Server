19b7615270943ddc35138127064c33f4d986a859
EunjunOh123 <grace99985@gmail.com>
2022-12-09 18:05:42 +0900

    feat: BusinessException 상속 받는 관계로 설정

--- src/main/java/com/gaethering/gaetheringserver/board/exception/PostException.java
+++ src/main/java/com/gaethering/gaetheringserver/board/exception/PostException.java
@@ -1,15 +1,16 @@
 package com.gaethering.gaetheringserver.board.exception;
 
 import com.gaethering.gaetheringserver.board.exception.errorCode.PostErrorCode;
+import com.gaethering.gaetheringserver.core.exception.BusinessException;
 import lombok.Getter;
 
 @Getter
-public class PostException extends RuntimeException{
+public class PostException extends BusinessException {
 
     private final PostErrorCode postErrorCode;
 
     protected PostException (PostErrorCode postErrorCode) {
-        super(postErrorCode.getMessage());
+        super(postErrorCode);
         this.postErrorCode = postErrorCode;
     }
 }
