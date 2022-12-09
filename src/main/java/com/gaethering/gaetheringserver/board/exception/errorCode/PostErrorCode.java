3d17dc99c979e2448eba0d9dfb028cfdedcac89a
EunjunOh123 <grace99985@gmail.com>
2022-12-09 17:04:55 +0900

    feat: 카테고리 존재하지 않는 경우의 에러코드 추가

--- src/main/java/com/gaethering/gaetheringserver/board/exception/errorCode/PostErrorCode.java
+++ src/main/java/com/gaethering/gaetheringserver/board/exception/errorCode/PostErrorCode.java
@@ -8,7 +8,9 @@ import lombok.Getter;
 public enum PostErrorCode {
 
     POST_NOT_EXIST("E801", "존재하지 않는 게시물입니다."),
-    CANNOT_UPDATE_POST("E802", "게시물 수정 권한은 작성자에게 있습니다.");
+
+    CATEGORY_NOT_EXIST("E802", "존재하지 않는 카테고리입니다."),
+    CANNOT_UPDATE_POST("E803", "게시물 수정 권한은 작성자에게 있습니다.");
 
     private final String code;
     private final String message;
