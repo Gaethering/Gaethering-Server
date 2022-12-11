656874a1d6be267ceac5719df845e7f4b59082d7
EunjunOh123 <grace99985@gmail.com>
2022-12-11 19:06:15 +0900

    feat: 댓글 요청/응답 dto 생성

---
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/dto/CommentResponse.java
@@ -0,0 +1,19 @@
+package com.gaethering.gaetheringserver.domain.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CommentResponse {
+
+    private String comment;
+
+    private String nickname;
+
+    private LocalDateTime createAt;
+}
