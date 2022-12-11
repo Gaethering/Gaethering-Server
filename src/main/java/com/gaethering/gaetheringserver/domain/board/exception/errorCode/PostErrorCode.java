c962fed30ba0ed79f8c43b6bf4eb9090ff71a2ff
EunjunOh123 <grace99985@gmail.com>
2022-12-12 01:18:11 +0900

    feat: 댓글 관련 예외 에러 코드 추가

--- src/main/java/com/gaethering/gaetheringserver/domain/board/exception/errorCode/PostErrorCode.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/exception/errorCode/PostErrorCode.java
@@ -10,7 +10,10 @@ public enum PostErrorCode implements ErrorCode {
 
     POST_NOT_EXIST("E801", "존재하지 않는 게시물입니다."),
     CATEGORY_NOT_EXIST("E802", "존재하지 않는 카테고리입니다."),
-    CANNOT_UPDATE_POST("E802", "게시물 수정 권한은 작성자에게 있습니다.");
+    COMMENT_NOT_EXIST("E803", "존재하지 않는 댓글입니다."),
+    CANNOT_UPDATE_COMMENT("E804", "댓글 수정 권한은 작성자에게 있습니다."),
+    CANNOT_DELETE_COMMENT("E805", "댓글 삭제 권한은 작성자에게 있습니다."),
+    CANNOT_UPDATE_POST("E806", "게시물 수정 권한은 작성자에게 있습니다.");
 
     private final String code;
     private final String message;
