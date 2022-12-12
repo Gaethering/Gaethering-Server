bfa1211ad9bb66e587dd5adf24284e0fc9bcb529
bokyung <qhrud7915@naver.com>
2022-12-12 21:15:02 +0900

    test: 게시글 수정 Service Test 코드 작성

--- src/test/java/com/gaethering/gaetheringserver/domain/board/service/PostServiceTest.java
+++ src/test/java/com/gaethering/gaetheringserver/domain/board/service/PostServiceTest.java
@@ -1,5 +1,6 @@
 package com.gaethering.gaetheringserver.domain.board.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
@@ -12,17 +13,24 @@ import static org.mockito.Mockito.verify;
 import com.gaethering.gaetheringserver.domain.aws.s3.S3Service;
 import com.gaethering.gaetheringserver.domain.board.dto.PostRequest;
 import com.gaethering.gaetheringserver.domain.board.dto.PostResponse;
+import com.gaethering.gaetheringserver.domain.board.dto.PostUpdateRequest;
+import com.gaethering.gaetheringserver.domain.board.dto.PostUpdateResponse;
 import com.gaethering.gaetheringserver.domain.board.entity.Category;
 import com.gaethering.gaetheringserver.domain.board.entity.Post;
+import com.gaethering.gaetheringserver.domain.board.entity.PostImage;
 import com.gaethering.gaetheringserver.domain.board.exception.CategoryNotFoundException;
+import com.gaethering.gaetheringserver.domain.board.exception.NoPermissionUpdatePostException;
+import com.gaethering.gaetheringserver.domain.board.exception.PostNotFoundException;
 import com.gaethering.gaetheringserver.domain.board.exception.errorCode.PostErrorCode;
 import com.gaethering.gaetheringserver.domain.board.repository.CategoryRepository;
+import com.gaethering.gaetheringserver.domain.board.repository.HeartRepository;
 import com.gaethering.gaetheringserver.domain.board.repository.PostRepository;
 import com.gaethering.gaetheringserver.domain.member.entity.Member;
 import com.gaethering.gaetheringserver.domain.member.exception.errorcode.MemberErrorCode;
 import com.gaethering.gaetheringserver.domain.member.exception.member.MemberNotFoundException;
 import com.gaethering.gaetheringserver.domain.member.repository.member.MemberRepository;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
@@ -46,6 +54,8 @@ class PostServiceTest {
     private PostRepository postRepository;
     @Mock
     private CategoryRepository categoryRepository;
+    @Mock
+    private HeartRepository heartRepository;
     @Mock
     private S3Service s3Service;
     @InjectMocks
@@ -146,11 +156,144 @@ class PostServiceTest {
         ArgumentCaptor<Post> captor = ArgumentCaptor.forClass(Post.class);
 
         PostResponse response
-            = postService.writePost(anyString(), null, request);
+            = postService.writePost(anyString(), new ArrayList<>(), request);
 
         assertEquals(0, response.getImageUrls().size());
         assertEquals("닉네임", response.getNickname());
         assertEquals("제목입니다", response.getTitle());
         verify(postRepository, times(1)).save(captor.capture());
+    }
+
+    @Test
+    @DisplayName("게시글 수정 실패-회원 찾을 수 없는 경우")
+    void updatePostFailure_MemberNotFound() {
+        // given
+        given(memberRepository.findByEmail(anyString()))
+            .willReturn(Optional.empty());
+
+        PostUpdateRequest request = PostUpdateRequest.builder()
+            .title("게시글 제목 수정")
+            .content("게시글 내용 수정")
+            .build();
+
+        // when
+        MemberNotFoundException exception = assertThrows(MemberNotFoundException.class,
+            () -> postService.updatePost("test@test.com", anyLong(), request));
+
+        // then
+        assertThat(exception.getErrorCode()).isEqualTo(MemberErrorCode.MEMBER_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("게시글 수정 실패-게시물 찾을 수 없는 경우")
+    void updatePostFailure_PostNotFound() {
+        // given
+        Member member = Member.builder()
+            .id(1L)
+            .email("gaethering@gmail.com")
+            .build();
+        given(memberRepository.findByEmail(anyString()))
+            .willReturn(Optional.of(member));
+        given(postRepository.findById(anyLong()))
+            .willReturn(Optional.empty());
+
+        PostUpdateRequest request = PostUpdateRequest.builder()
+            .title("게시글 제목 수정")
+            .content("게시글 내용 수정")
+            .build();
+
+        // when
+        PostNotFoundException exception = assertThrows(PostNotFoundException.class,
+            () -> postService.updatePost(member.getEmail(), 1L, request));
+
+        // then
+        assertThat(exception.getErrorCode()).isEqualTo(PostErrorCode.POST_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("게시글 수정 실패-게시물 작성자가 아닐 경우")
+    void updatePostFailure_NoPermissionUpdatePost() {
+        // given
+        Member member1 = Member.builder()
+            .id(1L)
+            .email("gaethering@gmail.com")
+            .build();
+        Member member2 = Member.builder()
+            .id(2L)
+            .email("test@gmail.com")
+            .build();
+        Post post = Post.builder()
+            .id(1L)
+            .title("게시물 제목")
+            .content("게시물 내용")
+            .member(member1)
+            .build();
+        given(memberRepository.findByEmail(anyString()))
+            .willReturn(Optional.of(member1));
+        given(memberRepository.findByEmail(anyString()))
+            .willReturn(Optional.of(member2));
+        given(postRepository.findById(anyLong()))
+            .willReturn(Optional.of(post));
+
+        PostUpdateRequest request = PostUpdateRequest.builder()
+            .title("게시글 제목 수정")
+            .content("게시글 내용 수정")
+            .build();
+
+        // when
+        NoPermissionUpdatePostException exception = assertThrows(NoPermissionUpdatePostException.class,
+            () -> postService.updatePost(member2.getEmail(), 1L, request));
+
+        // then
+        assertThat(exception.getErrorCode()).isEqualTo(PostErrorCode.NO_PERMISSION_TO_UPDATE_POST);
+    }
+
+    @Test
+    @DisplayName("게시글 수정 성공")
+    void updatePostSuccess() {
+        // given
+        Member member1 = Member.builder()
+            .id(1L)
+            .email("gaethering@gmail.com")
+            .nickname("닉네임")
+            .build();
+        Category category = Category.builder()
+            .id(1L)
+            .categoryName("질문 있어요")
+            .build();
+        PostImage postImage = PostImage.builder()
+            .id(1L)
+            .isRepresentative(false)
+            .imageUrl("https://test~")
+            .build();
+        Post post = Post.builder()
+            .id(1L)
+            .title("게시물 제목")
+            .content("게시물 내용")
+            .member(member1)
+            .postImages(List.of(postImage))
+            .category(category)
+            .build();
+
+        given(memberRepository.findByEmail(anyString()))
+            .willReturn(Optional.of(member1));
+        given(postRepository.findById(anyLong()))
+            .willReturn(Optional.of(post));
+        given(heartRepository.countByPost(post))
+            .willReturn(1L);
+
+        PostUpdateRequest request = PostUpdateRequest.builder()
+            .title("게시글 제목 수정")
+            .content("게시글 내용 수정")
+            .build();
+
+        // when
+        PostUpdateResponse response = postService.updatePost(member1.getEmail(), 1L, request);
+
+        // then
+        assertThat(response.getTitle()).isEqualTo(post.getTitle());
+        assertThat(response.getContent()).isEqualTo(post.getContent());
+        assertThat(response.getCreatedAt()).isEqualTo(post.getCreatedAt());
+        assertThat(response.getUpdatedAt()).isEqualTo(post.getUpdatedAt());
     }
 }
