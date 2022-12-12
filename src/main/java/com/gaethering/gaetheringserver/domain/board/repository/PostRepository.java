581883061fec6e0c421378dfc69932d175f7d018
naekang <rlawlsgh6306@gmail.com>
2022-12-12 17:15:25 +0900

    feat: member로 게시글 조회 Repository 메서드 작성

--- src/main/java/com/gaethering/gaetheringserver/domain/board/repository/PostRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/repository/PostRepository.java
@@ -1,8 +1,12 @@
 package com.gaethering.gaetheringserver.domain.board.repository;
 
 import com.gaethering.gaetheringserver.domain.board.entity.Post;
+import com.gaethering.gaetheringserver.domain.member.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByMember(Member member);
 
 }
