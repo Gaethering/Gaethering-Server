08cf830140b35ae9661a246f9651ada8e5cff60a
bokyung <qhrud7915@naver.com>
2022-12-12 20:43:56 +0900

    feat: 게시글 수정 Controller 작성

--- src/main/java/com/gaethering/gaetheringserver/domain/board/controller/PostController.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/controller/PostController.java
@@ -2,6 +2,8 @@ package com.gaethering.gaetheringserver.domain.board.controller;
 
 import com.gaethering.gaetheringserver.domain.board.dto.PostRequest;
 import com.gaethering.gaetheringserver.domain.board.dto.PostResponse;
+import com.gaethering.gaetheringserver.domain.board.dto.PostUpdateRequest;
+import com.gaethering.gaetheringserver.domain.board.dto.PostUpdateResponse;
 import com.gaethering.gaetheringserver.domain.board.service.PostService;
 import java.security.Principal;
 import java.util.List;
@@ -10,7 +12,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,5 +39,12 @@ public class PostController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
             .contentType(MediaType.APPLICATION_JSON).body(response);
+    }
+
+    @PatchMapping("/boards/{postId}")
+    public ResponseEntity<PostUpdateResponse> updatePost(@PathVariable Long postId,
+        @RequestBody PostUpdateRequest request, Principal principal) {
+
+        return ResponseEntity.ok(postService.updatePost(principal.getName(), postId, request));
     }
 }
