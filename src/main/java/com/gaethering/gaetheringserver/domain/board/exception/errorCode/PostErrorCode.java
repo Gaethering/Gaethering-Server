65a6cea489200413cba9f8973d1e864c9cb50362
bokyung <qhrud7915@naver.com>
2022-12-13 23:47:03 +0900

    feat: 게시글 이미지 삭제시 예외처리

--- src/main/java/com/gaethering/gaetheringserver/domain/board/exception/errorCode/PostErrorCode.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/exception/errorCode/PostErrorCode.java
@@ -13,7 +13,8 @@ public enum PostErrorCode implements ErrorCode {
     COMMENT_NOT_FOUND("E803", "존재하지 않는 댓글입니다."),
     NO_PERMISSION_TO_UPDATE_COMMENT("E804", "댓글 수정 권한은 작성자에게 있습니다."),
     NO_PERMISSION_TO_DELETE_COMMENT("E805", "댓글 삭제 권한은 작성자에게 있습니다."),
-    NO_PERMISSION_TO_UPDATE_POST("E806", "게시물 수정 권한은 작성자에게 있습니다.");
+    NO_PERMISSION_TO_UPDATE_POST("E806", "게시물 수정 권한은 작성자에게 있습니다."),
+    POST_IMAGE_NOT_FOUND("E807", "해당 게시물 이미지가 존재하지 않습니다.");
 
     private final String code;
     private final String message;
