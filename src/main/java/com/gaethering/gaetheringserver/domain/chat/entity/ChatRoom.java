cfa10cb8ee1eff07c96b5ecec8e7571ea43ec3e3
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-13 17:30:22 +0900

    feat: chat 관련 entity 추가

---
+++ src/main/java/com/gaethering/gaetheringserver/domain/chat/entity/ChatRoom.java
@@ -0,0 +1,48 @@
+package com.gaethering.gaetheringserver.domain.chat.entity;
+
+import com.gaethering.gaetheringserver.core.entity.BaseTimeEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChatRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "chatroom_id", nullable = false)
+    private Long id;
+
+    private String key;
+    private String description;
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<WalkingTime> walkingTimes = new ArrayList<>();
+
+    @OneToMany(mappedBy = "chatRoom")
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
+    public void addWalkingTime(WalkingTime walkingTime) {
+        walkingTimes.add(walkingTime);
+        walkingTime.setChatRoom(this);
+    }
+
+    public void addChatMessage(ChatMessage chatMessage) {
+        chatMessages.add(chatMessage);
+        chatMessage.setChatRoom(this);
+    }
+}
