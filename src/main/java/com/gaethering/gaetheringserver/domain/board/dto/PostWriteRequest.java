d6eb3e27ae2f717b284d597b53f867a30d3e376f
EunjunOh123 <grace99985@gmail.com>
2022-12-15 12:22:46 +0900

    fix: 게시물 작성 응답/요청 dto 클래스명 수정

--- src/main/java/com/gaethering/gaetheringserver/domain/board/dto/PostWriteRequest.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/dto/PostWriteRequest.java
@@ -1,32 +1,25 @@
 package com.gaethering.gaetheringserver.domain.board.dto;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class PostResponse {
+public class PostWriteRequest {
 
+    @NotBlank
     private String title;
 
+    @NotBlank
     private String content;
 
-    private List<String> imageUrls;
+    private Long categoryId;
 
-    private String categoryName;
-
-    private int viewCnt;
-
-    private int heartCnt;
-
-    private String nickname;
-
-    private LocalDateTime createAt;
 }
