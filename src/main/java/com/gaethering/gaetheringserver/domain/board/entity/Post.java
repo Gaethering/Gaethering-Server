4cea846ac357510f9d0a67f5efa9364b2ec8ce9f
EunjunOh123 <grace99985@gmail.com>
2022-12-19 13:14:54 +0900

    feat: post 엔티티에 좋아요 기능 구현 위한 메서드 생성

--- src/main/java/com/gaethering/gaetheringserver/domain/board/entity/Post.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/entity/Post.java
@@ -53,7 +53,9 @@ public class Post extends BaseTimeEntity {
         orphanRemoval = true)
     private List<PostImage> postImages = new ArrayList<>();
 
-    @OneToMany(mappedBy = "post")
+    @OneToMany(mappedBy = "post",
+            cascade = CascadeType.REMOVE,
+            orphanRemoval = true)
     private List<Heart> hearts = new ArrayList<>();
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -70,5 +72,13 @@ public class Post extends BaseTimeEntity {
     public void updatePost(String title, String content) {
         this.title = title;
         this.content = content;
+    }
+
+    public void pushPostHeart (Heart heart) {
+        this.hearts.add(heart);
+    }
+
+    public void cancelPostHeart (Heart heart) {
+        this.hearts.remove(heart);
     }
 }
