76789b7275b437c4a71efe992c44b8028b0a8a4c
bokyung <qhrud7915@naver.com>
2022-12-19 17:38:29 +0900

    feat: 게시글 이미지 조회 repository 추가

--- src/main/java/com/gaethering/gaetheringserver/domain/board/repository/PostImageRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/repository/PostImageRepository.java
@@ -1,8 +1,11 @@
 package com.gaethering.gaetheringserver.domain.board.repository;
 
+import com.gaethering.gaetheringserver.domain.board.entity.Post;
 import com.gaethering.gaetheringserver.domain.board.entity.PostImage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostImageRepository extends JpaRepository<PostImage, Long> {
 
+	List<PostImage> findAllByPost(Post post);
 }
