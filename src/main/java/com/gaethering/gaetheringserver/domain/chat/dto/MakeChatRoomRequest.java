3ad250a55730afe3a43df6b9080363140df4c7ac
naekang <rlawlsgh6306@gmail.com>
2022-12-19 17:26:33 +0900

    feat: 채팅방 만들기 요청 DTO 추가

---
+++ src/main/java/com/gaethering/gaetheringserver/domain/chat/dto/MakeChatRoomRequest.java
@@ -0,0 +1,14 @@
+package com.gaethering.gaetheringserver.domain.chat.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MakeChatRoomRequest {
+    private String description;
+}
