133d0341402590c395809f095d955249f1cb525d
naekang <rlawlsgh6306@gmail.com>
2022-12-19 18:47:26 +0900

    feat: 채팅방 만들기 DTO Entity -> Custom DTO

--- src/main/java/com/gaethering/gaetheringserver/domain/chat/dto/MakeChatRoomRequest.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/chat/dto/MakeChatRoomRequest.java
@@ -14,5 +14,5 @@ import lombok.NoArgsConstructor;
 public class MakeChatRoomRequest {
 
     private String description;
-    private List<WalkingTime> walkingTimes;
+    private List<WalkingTimeInfo> walkingTimes;
 }
