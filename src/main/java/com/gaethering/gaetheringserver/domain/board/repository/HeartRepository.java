d6d1ebf1c9b2674e333fea3f2d868018fc846b4c
EunjunOh123 <grace99985@gmail.com>
2022-12-19 13:21:04 +0900

    feat: 좋아요 repository 메서드 추가

--- src/main/java/com/gaethering/gaetheringserver/domain/board/repository/HeartRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/repository/HeartRepository.java
@@ -2,9 +2,16 @@ package com.gaethering.gaetheringserver.domain.board.repository;
 
 import com.gaethering.gaetheringserver.domain.board.entity.Heart;
 import com.gaethering.gaetheringserver.domain.board.entity.Post;
+import com.gaethering.gaetheringserver.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
 	Long countByPost(Post post);
+
+	Optional<Heart> findByPostAndMember (Post post, Member member);
+
+	boolean existsByPostAndMember (Post post, Member member);
 }
