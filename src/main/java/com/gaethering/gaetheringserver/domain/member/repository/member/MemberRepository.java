56c43562133a6ac3721151f7a7e6087533b18cdf
bokyung <qhrud7915@naver.com>
2022-12-19 17:39:10 +0900

    feat: 게시글 삭제시 회원 조회 쿼리 작성

--- src/main/java/com/gaethering/gaetheringserver/domain/member/repository/member/MemberRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/member/repository/member/MemberRepository.java
@@ -1,9 +1,15 @@
 package com.gaethering.gaetheringserver.domain.member.repository.member;
 
 import com.gaethering.gaetheringserver.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
 
     boolean existsByEmail(String email);
+
+	@Query("select m from Member m where m.email = :email")
+	Optional<Member> findMembersByEmail(@Param("email")String email);
 }
