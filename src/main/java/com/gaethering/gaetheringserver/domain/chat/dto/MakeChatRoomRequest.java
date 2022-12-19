6e0f231699a90126bef2ad9598497cc656fe28b0
naekang <rlawlsgh6306@gmail.com>
2022-12-19 18:11:25 +0900

    feat: 채팅방 만들기 요청 DTO 산책 시간 필드 추가

--- src/main/java/com/gaethering/gaetheringserver/domain/chat/dto/MakeChatRoomRequest.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/chat/dto/MakeChatRoomRequest.java
@@ -1,5 +1,7 @@
 package com.gaethering.gaetheringserver.domain.chat.dto;
 
+import com.gaethering.gaetheringserver.domain.chat.entity.WalkingTime;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,5 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MakeChatRoomRequest {
+
     private String description;
+    private List<WalkingTime> walkingTimes;
 }
