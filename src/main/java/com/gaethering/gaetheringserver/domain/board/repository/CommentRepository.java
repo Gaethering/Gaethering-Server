c5fa93d326362b27176bf561f98ace916bbbfdc3
bokyung <qhrud7915@naver.com>
2022-12-19 17:43:43 +0900

    feat: 게시글 삭제시 댓글, 좋아요 삭제 쿼리 작성

--- src/main/java/com/gaethering/gaetheringserver/domain/board/repository/CommentRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/repository/CommentRepository.java
@@ -1,7 +1,15 @@
 package com.gaethering.gaetheringserver.domain.board.repository;
 
 import com.gaethering.gaetheringserver.domain.board.entity.Comment;
+import com.gaethering.gaetheringserver.domain.board.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+	@Modifying
+	@Query("delete from Comment c where c.post = :post")
+	void deleteCommentsAllByPostId(@Param("post") Post post);
 }
--- src/main/java/com/gaethering/gaetheringserver/domain/board/repository/HeartRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/repository/HeartRepository.java
@@ -3,8 +3,15 @@ package com.gaethering.gaetheringserver.domain.board.repository;
 import com.gaethering.gaetheringserver.domain.board.entity.Heart;
 import com.gaethering.gaetheringserver.domain.board.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
 	Long countByPost(Post post);
+
+	@Modifying
+	@Query("delete from Heart h where h.post = :post")
+	void deleteHeartAllByPostId(@Param("post") Post post);
 }
