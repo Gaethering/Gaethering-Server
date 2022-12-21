6f691687b5756122d3a9557214407ccfa9a352fb
naekang <rlawlsgh6306@gmail.com>
2022-12-21 14:17:44 +0900

    feat: 채팅방 삭제 시 산책 시간 삭제 벌크 연산 메서드 추가

--- src/main/java/com/gaethering/gaetheringserver/domain/chat/repository/WalkingTimeRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/chat/repository/WalkingTimeRepository.java
@@ -1,8 +1,16 @@
 package com.gaethering.gaetheringserver.domain.chat.repository;
 
+import com.gaethering.gaetheringserver.domain.chat.entity.ChatRoom;
 import com.gaethering.gaetheringserver.domain.chat.entity.WalkingTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface WalkingTimeRepository extends JpaRepository<WalkingTime, Long> {
+
+    @Modifying
+    @Query("delete from WalkingTime w where w.chatRoom = :chatRoom")
+    void deleteAllByChatRoomId(@Param("chatRoom") ChatRoom chatRoom);
 
 }
