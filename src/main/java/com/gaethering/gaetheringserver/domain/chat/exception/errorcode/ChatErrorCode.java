9e36dffaf49e3977458618048356ddd397e597c8
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-22 15:22:07 +0900

    feat(ChatRoomNotFoundException): 채팅방 못 찾았을때 예외 상황 처리

---
+++ src/main/java/com/gaethering/gaetheringserver/domain/chat/exception/errorcode/ChatErrorCode.java
@@ -0,0 +1,16 @@
+package com.gaethering.gaetheringserver.domain.chat.exception.errorcode;
+
+import com.gaethering.gaetheringserver.core.exception.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ChatErrorCode implements ErrorCode {
+
+    CHAT_ROOM_NOT_FOUND("E110", "해당 채팅방은 존재하지 않습니다.");
+
+    private final String code;
+    private final String message;
+
+}
