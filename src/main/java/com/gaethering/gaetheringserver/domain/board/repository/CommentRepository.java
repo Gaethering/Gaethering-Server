5666fa5804bb5c9c44e0e935c186739943b3860e
EunjunOh123 <grace99985@gmail.com>
2022-12-22 22:52:13 +0900

    feat: 게시물 내 댓글들 개수 구하는 메서드 및 댓글 리스트(with 페이징 처리) 가져오는 메서드 구현

--- src/main/java/com/gaethering/gaetheringserver/domain/board/repository/CommentRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/repository/CommentRepository.java
@@ -2,14 +2,21 @@ package com.gaethering.gaetheringserver.domain.board.repository;
 
 import com.gaethering.gaetheringserver.domain.board.entity.Comment;
 import com.gaethering.gaetheringserver.domain.board.entity.Post;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 	@Modifying
 	@Query("delete from Comment c where c.post = :post")
 	void deleteCommentsAllByPostId(@Param("post") Post post);
+
+	List<Comment> findAllByPostAndIdIsLessThanOrderByIdDesc(Post post, Long lastCommentId, PageRequest pageRequest);
+
+	long countByPost(Post post);
 }
