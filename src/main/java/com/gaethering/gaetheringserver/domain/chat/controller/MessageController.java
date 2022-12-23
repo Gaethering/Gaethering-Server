0c06f1138a63e3f40bc80835d3da62c555fad907
ChoiYoungHoon <dudgns2865@naver.com>
2022-12-23 13:03:30 +0900

    feat: message controller 구현

--- src/main/java/com/gaethering/gaetheringserver/domain/chat/controller/MessageController.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/chat/controller/MessageController.java
@@ -1,9 +1,11 @@
 package com.gaethering.gaetheringserver.domain.chat.controller;
 
 import com.gaethering.gaetheringserver.domain.chat.dto.ChatMessageRequest;
+import com.gaethering.gaetheringserver.domain.chat.dto.ChatMessageResponse;
 import com.gaethering.gaetheringserver.domain.chat.service.ChatMessageService;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,8 +15,13 @@ public class MessageController {
 
     private final ChatMessageService chatMessageService;
 
-    @MessageMapping("/chat.send")
-    public void send(@Valid ChatMessageRequest request) {
-        chatMessageService.send(request);
+    @MessageMapping("chat.enter.{roomKey}")
+    public ChatMessageResponse enter(@Valid ChatMessageRequest request, @DestinationVariable String roomKey) {
+        return chatMessageService.enter(request, roomKey);
+    }
+
+    @MessageMapping("chat.send.{roomKey}")
+    public ChatMessageResponse send(@Valid ChatMessageRequest request, @DestinationVariable String roomKey) {
+        return chatMessageService.send(request, roomKey);
     }
 }
