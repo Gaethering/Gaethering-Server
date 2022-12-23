52fdece0d614a1dc9e99230deb5be219b90508c4
EunjunOh123 <grace99985@gmail.com>
2022-12-23 18:06:04 +0900

    feat: 페이징 처리를 도와주는 클래스 생성

---
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/util/ScrollPagingUtil.java
@@ -0,0 +1,34 @@
+package com.gaethering.gaetheringserver.domain.board.util;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ScrollPagingUtil<T> {
+
+    private final List<T> itemsWithNextCursor;
+
+    private final int countPerScroll;
+
+    public static <T> ScrollPagingUtil<T> of(List<T> itemsWithNextCursor, int size) {
+        return new ScrollPagingUtil<>(itemsWithNextCursor, size);
+    }
+
+    public boolean isLastScroll() {
+        return this.itemsWithNextCursor.size() <= countPerScroll;
+    }
+
+    public List<T> getCurrentScrollItems() {
+        if (isLastScroll()) {
+            return this.itemsWithNextCursor;
+        }
+        return this.itemsWithNextCursor.subList(0, countPerScroll);
+    }
+
+    public T getNextCursor() {
+        return itemsWithNextCursor.get(countPerScroll - 1);
+    }
+
+}
