196f6385cec970982942d7675217d4334dca1085
EunjunOh123 <grace99985@gmail.com>
2022-12-26 01:54:26 +0900

    feat: 게시물 상세 조회 시 응답 dto 생성

---
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/dto/PostGetOneResponse.java
@@ -0,0 +1,40 @@
+package com.gaethering.gaetheringserver.domain.board.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PostGetOneResponse {
+
+    private Long postId;
+
+    private String title;
+
+    private String content;
+
+    private String nickname;
+
+    private long heartCnt;
+
+    private long viewCnt;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @JsonProperty(value = "isOwner")
+    private boolean owner;
+
+    private List<PostGetImageUrlResponse> images;
+
+    public void setOwner(boolean owner) {
+        this.owner = owner;
+    }
+}
