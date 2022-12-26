b8c9c81908aebe6228897c0c443b0f060d151ac4
EunjunOh123 <grace99985@gmail.com>
2022-12-26 17:41:46 +0900

    feat: 카테고리별 게시물 개수 및 카테고리별 게시물 리스트(with 페이징 처리) 가져오는 메서드 구현

--- src/main/java/com/gaethering/gaetheringserver/domain/board/repository/PostRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/repository/PostRepository.java
@@ -1,12 +1,18 @@
 package com.gaethering.gaetheringserver.domain.board.repository;
 
+import com.gaethering.gaetheringserver.domain.board.entity.Category;
 import com.gaethering.gaetheringserver.domain.board.entity.Post;
 import com.gaethering.gaetheringserver.domain.member.entity.Member;
 import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findAllByMember(Member member);
 
+    List<Post> findAllByCategoryAndIdIsLessThanOrderByIdDesc(Category category, long lastPostId, PageRequest pageRequest);
+
+    long countByCategory (Category category);
 }
