ca917ece240e68bc8d32a06985085cbee12ab5bc
EunjunOh123 <grace99985@gmail.com>
2022-12-26 18:28:39 +0900

    feat: querydsl 사용하여 게시물 상세 조회 시 조회수 증가 쿼리 작성

--- src/main/java/com/gaethering/gaetheringserver/domain/board/repository/PostRepository.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/board/repository/PostRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> , CustomPostRepository{
 
     List<Post> findAllByMember(Member member);
 
