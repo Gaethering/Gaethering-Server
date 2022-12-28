c74d7126e925e98c9422c92aa4877974d99ae0ec
naekang <rlawlsgh6306@gmail.com>
2022-12-29 00:27:58 +0900

    feat: 채팅방 조회 DTO 이름 수정

--- src/main/java/com/gaethering/gaetheringserver/domain/chat/dto/ChatRoomListResponse.java
+++ src/main/java/com/gaethering/gaetheringserver/domain/chat/dto/ChatRoomListResponse.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class LocalChatRoomResponse {
+public class ChatRoomListResponse {
 
     private int numberOfChatRooms;
 
